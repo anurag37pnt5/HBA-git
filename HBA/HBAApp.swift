e554d79c99a58d9a7c1d34774028b477d2ef710c
Anurag <anuragb375@gmail.com>
2024-03-22 22:46:21 -0400

    Initial Commit

---
+++ HBA/HBAApp.swift
@@ -0,0 +1,17 @@
+//
+//  HBAApp.swift
+//  HBA
+//
+//  Created by Anurag Baral on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct HBAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
